dfec2ad4051441251cff575a24b6266b54cde3e5
Toby Stayner <toby.stayner@gmail.com>
2022-08-07 15:55:17 +1200

    Add basic HTTP server

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * @author Toby Stayner <toby@swengineer.dev>
+ */
+
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const vhost = require("vhost");
+const path = require("path");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello, World!").status(200);
+});
+
+// virtual hosts
+var virtualHosts = JSON.parse(fs.readFileSync("vhosts.json", "utf8"));
+
+virtualHosts.forEach(function (virtualHost) {
+    var virtualHostApp = express();
+    virtualHostApp.use(express.static(path.join(__dirname, virtualHost.path)));
+    app.use(vhost(virtualHost.domain, virtualHostApp));
+});
+
+// create servers
+const localhost = "localhost";
+
+const httpPort = 8080;
+const httpServer = http.createServer(app);
+const httpListener = httpServer.listen(httpPort, localhost, () => {
+    const address = httpListener.address().address;
+    const port = httpListener.address().port;
+    console.log(`HTTP server listening at http://${address}:${port}/`);
+});
