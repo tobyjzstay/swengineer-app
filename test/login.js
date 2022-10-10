13d65023f69a88d29dd40095ebc8f4746af2515e
Toby <staynetoby@ecs.vuw.ac.nz>
2022-10-10 21:27:33 +1300

    Add more test cases for login page

--- test/login.js
+++ test/login.js
@@ -66,11 +66,167 @@ makeSuite("POST /login", () => {
     });
 });
 
-// makeSuite("POST /login", () => {
-//     it("Log in as invalid user", (done) => {
-//         request(app).post("/api/login").send({ email: "bob.smith@example.com", password: "bob" }).expect(404, done);
-//     });
-// });
+makeSuite("POST /login", () => {
+    it("Log in as invalid user", (done) => {
+        request(app).post("/api/login").send({ email: "bob.smith@example.com", password: "bob" }).expect(404, done);
+    });
+});
+
+makeSuite("GET /register/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    it("Verify email with verification token", (done) => {
+        request(app).get("/api/register/" + user.verificationToken).send().expect(200, done);
+    });
+});
+
+makeSuite("GET /register/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Verify email with invalid verification token", (done) => {
+        request(app).get("/api/register/thisisnotavalidtoken").send().expect(404, done);
+    });
+});
+
+makeSuite("POST /login", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    it("Verify email with verification token", (done) => {
+        request(app).get("/api/register/" + user.verificationToken).send().expect(200, done);
+    });
+
+    it("Log in as verified user", (done) => {
+        request(app).post("/api/login").send({ email: "alice@example.com", password: "alice" }).expect(404, done);
+    });
+    // TODO: check cookie?
+});
+
+makeSuite("POST /reset", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+});
+
+makeSuite("POST /reset", () => {
+    it("Send reset password email to invalid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(404, done);
+    });
+});
+
+makeSuite("GET /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    it("Check reset password link valid", (done) => {
+        request(app).get("/api/reset/" + user.resetPasswordToken).send().expect(200, done);
+    });
+});
+
+makeSuite("GET /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    it("Check reset password link invalid", (done) => {
+        request(app).get("/api/reset/thisisnotavalidtoken").send().expect(404, done);
+    });
+});
+
+makeSuite("GET /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    // make token expired
+    user.resetPasswordExpires = Date(0);
+    user.save();
+
+    it("Check expired reset password link invalid", (done) => {
+        request(app).get("/api/reset/" + user.resetPasswordToken).send().expect(401, done);
+    });
+});
+
+makeSuite("POST /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    it("Check reset password link valid", (done) => {
+        request(app).get("/api/reset/" + user.resetPasswordToken).send({}).expect(200, done);
+    });
+});
+
+makeSuite("POST /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    it("Check reset password link invalid", (done) => {
+        request(app).get("/api/reset/thisisnotavalidtoken").send().expect(404, done);
+    });
+});
+
+makeSuite("POST /reset/:token", () => {
+    it("Register a new user", (done) => {
+        request(app).post("/api/register").send({ email: "alice@example.com", password: "alice" }).expect(200, done);
+    });
+
+    it("Send reset password email to valid user", (done) => {
+        request(app).post("/api/reset").send({ email: "alice@example.com" }).expect(200, done);
+    });
+
+    const user = getUser("alice@example.com");
+
+    // make token expired
+    user.resetPasswordExpires = Date(0);
+    user.save();
+
+    it("Check expired reset password link invalid", (done) => {
+        request(app).get("/api/reset/" + user.resetPasswordToken).send().expect(401, done);
+    });
+});
+
 
 function makeSuite(name, tests) {
     describe(name, () => {
@@ -83,6 +239,12 @@ function makeSuite(name, tests) {
         after((done) => {
             done();
         });
+    });
+}
+
+function getUser(email) {
+    User.findOne({ email: email }, function (err, user) {
+        return user;
     });
 }
 
