87448beb7684cd146c3e9a3ef8b687918d33a9ee
Toby Stayner <toby.stayner@gmail.com>
2022-11-14 22:54:02 +1300

    Fix workflow + secrets

--- decrypt.sh
+++ decrypt.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$ENV_SECRET" \
+--output .env .env.gpg
