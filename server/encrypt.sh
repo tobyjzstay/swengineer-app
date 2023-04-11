0f5bcc3b533190519611d8fd5c67facf66e03b99
Toby Stayner <toby@swengineer.dev>
2023-04-12 08:47:00 +1200

    Remove workflow encryption

--- server/encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source .env
-# Encrypt the file
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase="$ENV_SECRET" .env
