a766915ebf8a6ef4c7206fc15ba038c3c30723a0
Toby Stayner <toby@swengineer.dev>
2023-04-14 18:52:11 +1200

    Refactor clock code

--- client/src/routes/Clock.tsx
+++ client/src/routes/Clock.tsx
@@ -1,96 +1,70 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
-import { Appbar } from "../components/Appbar";
+
+const MICROSECOND = 1 / 1000;
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const YEAR = DAY * 365.2425;
+
+const START_TIME = new Date("November 11, 2014").getTime();
+const TIME_DILATION = (7 * YEAR) / HOUR;
+const UPDATE_RATE = Math.floor(minUnit(1 / TIME_DILATION));
 
 export function Clock() {
+    const timeRef = React.useRef<HTMLDivElement>(null);
+
     React.useEffect(() => {
-        const element = document.getElementById("millerTime");
-
-        const microsecond = 1 / 1000;
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-        const year = day * 365.2425;
-
-        const startDate = new Date("November 11, 2014");
-        const timeRatio = (7 * year) / hour;
-
-        // let shift = 0;
-        const ms = Math.floor(minUnit(1 / timeRatio));
-
-        function minUnit(ms: number): number {
-            if (ms >= 1000) {
-                // shift--;
-                return minUnit(ms / 1000);
-            } else if (ms < 1) {
-                // shift++;
-                return minUnit(ms * 1000);
-            }
-            return ms;
-        }
-
-        function updateMillerTime() {
-            if (!element) return;
-
-            const endDate = new Date();
-            const timeElapsed = endDate.getTime() - startDate.getTime();
-            const ratioTimeElapsed = timeElapsed / timeRatio;
-
-            const microseconds = Math.floor(Math.floor((ratioTimeElapsed / microsecond) % 1000));
-            const milliseconds = Math.floor(ratioTimeElapsed % 1000);
-            const seconds = Math.floor((ratioTimeElapsed / second) % 60);
-            const minutes = Math.floor((ratioTimeElapsed / minute) % 60);
-            const hours = Math.floor((ratioTimeElapsed / hour) % 24);
-            const days = Math.floor(Math.floor(ratioTimeElapsed / day) % 365.2425);
-            const years = Math.floor(ratioTimeElapsed / year);
-
-            const stringYears = years ? `${years}y ` : "";
-            const stringDays = stringYears.length || days ? `${days}d ` : "";
-            const stringHours = stringDays.length || hours ? `${hours}h ` : "";
-            const stringMinutes = stringHours.length || minutes ? `${minutes}m ` : "";
-            const stringSeconds = stringMinutes.length || seconds ? `${seconds}s ` : "";
-            const stringMilliseconds =
-                stringSeconds.length || milliseconds
-                    ? microseconds
-                        ? `${padLeadingZeros(milliseconds, 3)}ms `
-                        : `${milliseconds}ms `
-                    : "";
-            const stringMicroseconds =
-                stringMilliseconds.length || microseconds ? `${padLeadingZeros(microseconds, 3)}μs` : "";
-
-            element.textContent =
-                stringYears +
-                stringDays +
-                stringHours +
-                stringMinutes +
-                stringSeconds +
-                stringMilliseconds +
-                (Math.floor(timeRatio / 1000) > 1 && Math.floor(timeRatio / 1000) < 1000 ? stringMicroseconds : "");
-        }
-
-        if (!element) return;
-        setInterval(updateMillerTime, ms);
+        const element = timeRef.current;
+        setInterval(() => tick(element), UPDATE_RATE);
     }, []);
 
     return (
-        <>
-            <Appbar />
-            <Container
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    minHeight: "100vh",
-                }}
-            >
-                <Typography id="millerTime" component="h1" variant="h1" />
-                <Typography variant="subtitle1">{"have passed on Miller's planet"}</Typography>
-            </Container>
-        </>
+        <Container
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                minHeight: "100vh",
+            }}
+        >
+            <Typography ref={timeRef} component="h1" variant="h1" />
+            <Typography variant="subtitle1">{"have passed on Miller's planet"}</Typography>
+        </Container>
     );
+}
+
+function tick(element: HTMLDivElement | null) {
+    if (!element) return;
+    const timeElapsed = new Date().getTime() - START_TIME;
+    const ratioTimeElapsed = timeElapsed / TIME_DILATION;
+
+    const microseconds = Math.floor(Math.floor((ratioTimeElapsed / MICROSECOND) % 1000));
+    const milliseconds = Math.floor(ratioTimeElapsed % 1000);
+    const seconds = Math.floor((ratioTimeElapsed / SECOND) % 60);
+    const minutes = Math.floor((ratioTimeElapsed / MINUTE) % 60);
+    const hours = Math.floor((ratioTimeElapsed / HOUR) % 24);
+    const days = Math.floor(Math.floor(ratioTimeElapsed / DAY) % 365.2425);
+    const years = Math.floor(ratioTimeElapsed / YEAR);
+
+    const strYears = years ? `${years}y ` : "";
+    const strDays = strYears.length || days ? `${days}d ` : "";
+    const strHours = strDays.length || hours ? `${hours}h ` : "";
+    const strMinutes = strHours.length || minutes ? `${minutes}m ` : "";
+    const strSeconds = strMinutes.length || seconds ? `${seconds}s ` : "";
+    const strMilliseconds = strSeconds.length || milliseconds ? `${milliseconds}ms ` : "";
+    const strMicroseconds = strMilliseconds.length || microseconds ? `${padLeadingZeros(microseconds, 3)}μs` : "";
+
+    element.textContent = strYears + strDays + strHours + strMinutes + strSeconds + strMilliseconds + strMicroseconds;
+}
+
+function minUnit(ms: number): number {
+    if (ms >= 1000) return minUnit(ms / 1000);
+    else if (ms < 1) return minUnit(ms * 1000);
+    else return ms;
 }
 
 function padLeadingZeros(num: number, size: number) {
